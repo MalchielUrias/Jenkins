aab9a3379d03e56cad8c47a16a5e7038c77277c4
MalchielUrias <malchieluriasbusiness@gmail.com>
2022-09-12 21:57:49 +0100

    changes to test poll. error correction

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingTalk() {
     String someone = "World, Thanks you making me Happy. I really appreciate you.";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
