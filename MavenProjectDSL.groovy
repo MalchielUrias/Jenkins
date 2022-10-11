1049a2ac41b5b8312c78d2ba9ec2e23622a39a92
MalchielUrias <malchieluriasbusiness@gmail.com>
2022-10-11 19:42:25 +0100

    Correction

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -1,7 +1,7 @@
 job('First-Maven-Project-Job-DSL'){
     description("First Maven DSL job on ${new Date()}")
     scm{
-        git("https://github.com/MalchielUrias/Jenkins.git", main)
+        git("https://github.com/MalchielUrias/Jenkins.git", 'main')
     }
     triggers{
         scm('* * * * *')
